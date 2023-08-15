3cc4c6081221c3ff5a3ee3f7a81a2b65f44ee5c6
Tkok <murdereruu@yandex.ru>
2023-08-15 15:27:21 +0300

    revision and correction

--- src/sitellite.hpp
+++ src/sitellite.hpp
@@ -4,102 +4,87 @@
 #include <Eigen/Dense>
 #include <cmath>
 
-constexpr double J2 = 1.082'626'68 * 1e-3;
-constexpr double Re = 6'378'137;
-constexpr double GM = 3.986'004'418'888'88 * 1e14;  
-
+using Vector2d = Eigen::Vector<double, 2>;
 
 struct Cartesian{
-    Eigen::Vector3d position;
-    Eigen::Vector3d velocity;
+    Vector2d position;
+    Vector2d velocity;
 };
-
 
 struct Keplerian{
-    double a;
-    double e;
-    double w;
-    double nu;
+    double sem_axis;
+    double eccentricity;
+    double arg_peri;
+    double anomaly;
 };
 
 
-Cartesian KeplerianToCartesian(const Keplerian& K, const double g){
-    double phi = K.w + K.nu;
-    Eigen::Vector3d r;
-    Eigen::Vector3d v;
+Cartesian toCartesian(const Keplerian& keplerian, const double gravParam) noexcept{
+    const double param = keplerian.sem_axis * (1 - keplerian.eccentricity * keplerian.eccentricity);
+    const double anomalyPlusw = keplerian.anomaly + keplerian.arg_peri;
+    const double cos_anomaly = std::cos(keplerian.anomaly);
+    const double sin_anomaly = std::sin(keplerian.anomaly);
 
-    r[0] = K.a * (1 - K.e * K.e) * std::cos(phi) / (1. + K.e * std::cos(K.nu));
-    r[1] = K.a * (1 - K.e * K.e) * std::sin(phi) / (1. + K.e * std::cos(K.nu));
+    const double R = param / (1 + keplerian.eccentricity * cos_anomaly);
+    const double x = R * cos_anomaly;
+    const double y = R * sin_anomaly;
 
-    double R = r.norm();
-    double V = std::sqrt(g * (2. / R - 1. / K.a));
-    double h = std::sqrt(g * K.a * (1 - K.e * K.e));
-    double fpa = acos(h / (R * V));
-    if (K.nu > M_PI)
-        fpa = 2 * M_PI - fpa;
+    const double muDivPsqrt = sqrt(gravParam / param);    
+    const double Vx = -muDivPsqrt * sin_anomaly;
+    const double Vy = muDivPsqrt * (keplerian.eccentricity + cos_anomaly);
     
-    v[0] = V * sin(fpa - phi);
-    v[1] = V * cos(phi - fpa);
-
-    return Cartesian{r,v};
+    const double c = std::cos(keplerian.arg_peri);
+    const double s = std::sin(keplerian.arg_peri);
+    return {{x*c - y*s, y*c + x*s},{Vx*c - Vy*s, Vy*c + Vx*s}};
 }
 
-Keplerian CartesianToKeplerian(const Cartesian& C, const double g){
 
-    double h = C.position[0] * C.velocity[1] - C.position[1] * C.velocity[0];
-    double R = C.position.norm();
-    double V = C.velocity.norm();
+Keplerian toKeplerian(const Cartesian& cartesian, const double gravParam) noexcept {
+	const double velSqr = cartesian.velocity.squaredNorm();
+	const double vel = std::sqrt(velSqr);
+	const double positionNorm = cartesian.position.norm();
+	const double muDivR = gravParam / positionNorm;
+	const Vector2d eccVector = ((velSqr - muDivR) * cartesian.position - cartesian.velocity.dot(cartesian.position) * cartesian.velocity) / gravParam;
+	const double ecc = eccVector.norm();
+	const double periapsisArgument = (ecc != 0) ? std::atan2(eccVector.y(), eccVector.x()) : 0;
 
-    double a = 1. / (2. / R - V * V / g);
-    double e = std::sqrt(1. - h * h / g / a);
-    double alpha = atan2(C.position[1], C.position[0]) + 2 * M_PI; // = w + nu
+	const Vector2d e1 = ecc != 0 ? eccVector / ecc : Vector2d{1, 0};
+	const Vector2d e2(-e1.y(), e1.x());
+	const double anomalyly = std::atan2(cartesian.position.dot(e2), cartesian.position.dot(e1));
+	const double semimajor = gravParam / (2 * muDivR - velSqr);
     
-    double nu = acos((a * (1 - e * e) / R - 1) / e);
-    double w = alpha - nu;
-    Keplerian K{a,e,w,nu};
-    Cartesian new_C = KeplerianToCartesian(K,g); 
-    double delta =  (C.velocity - new_C.velocity).norm() + (C.position - new_C.position).norm();
+	return {semimajor, ecc, periapsisArgument, anomalyly};
+}
+
+
+const Keplerian maneuver(const Cartesian& cartesian, const Eigen::Vector2d& delta_velocity, const double gravParam) noexcept{
+    Cartesian new_cartesian = cartesian;
+    new_cartesian.velocity += delta_velocity;    
+    Keplerian keplerian = toKeplerian(new_cartesian, gravParam);
     
-    if (delta > 1e-7)  
-        K.nu = 2 * M_PI - K.nu;
+    return keplerian;
+}
+
+
+const double precession_rate(const double sem_axis, const double ecc, const double i,
+                        const double Re, const double J2, const double gravParam) noexcept{
+    const double ReDivP = Re / sem_axis / (1 - ecc * ecc);
+    return -3. * ReDivP * ReDivP / sem_axis * std::sqrt(gravParam/sem_axis) * J2 * std::cos(i) / 2.;
+}
+
+
+double T_maneuver(const Cartesian& cartesian, const Eigen::Vector2d& dv, const double DeltaOmega,
+                const double i, const double Re, const double J2, const double gravParam) noexcept{
+    const Keplerian keplerian = toKeplerian(cartesian,gravParam);
+    const double omegaDot = precession_rate(keplerian.sem_axis, keplerian.eccentricity, i, Re, J2, gravParam);
+    
+    const Keplerian new_keplerian = maneuver(cartesian, dv, gravParam);
+    const double new_omegaDot = precession_rate(new_keplerian.sem_axis, new_keplerian.eccentricity, i, Re, J2, gravParam);
         
-    K.w = (alpha <= K.nu) ? alpha - K.nu + 2 * M_PI: alpha - K.nu;
+    const double delta_omegaDot = new_omegaDot - omegaDot;
+    const double k = (delta_omegaDot * DeltaOmega < 0) ? delta_omegaDot / std::abs(delta_omegaDot) : 0;
 
-    return K;
-}
-
-
-const Keplerian maneuver(const Cartesian& C, const Eigen::Vector3d& dv, const double g){
-    Cartesian new_C = C;
-    new_C.velocity += dv;
-    
-    Keplerian K = CartesianToKeplerian(new_C, g);
-    
-    return K;
-}
-
-const double precession_rate(const double a, const double e, const double i) {
-    return -3. * Re*Re / (a*a*a * std::sqrt(a) * (1-e*e)*(1-e*e)) * std::sqrt(GM) * J2 * std::cos(i) / 2.;
-}
-
-double T_maneuver(const Cartesian& C, const Eigen::Vector3d& dv, const double DeltaOmega, const double i){
-    Keplerian K = CartesianToKeplerian(C,GM);
-    double omegaDot = precession_rate(K.a, K.e, i);
-    
-    Keplerian new_K = maneuver(C, dv, GM);
-    double new_omegaDot = precession_rate(new_K.a, new_K.e, i);
-    
-
-    double delta_omegaDot = new_omegaDot - omegaDot;
-    if (delta_omegaDot * DeltaOmega > 0)
-        return DeltaOmega / delta_omegaDot;
-    
-    if (delta_omegaDot > 0 && DeltaOmega < 0)
-        return (2 * M_PI + DeltaOmega) / delta_omegaDot;
-    if (delta_omegaDot < 0 && DeltaOmega > 0)
-        return (- 2 * M_PI + DeltaOmega) / delta_omegaDot;
-
-    return 0.;
+    return (delta_omegaDot != 0) ? (DeltaOmega + k * 2 * M_PI) / delta_omegaDot : -1;
 }
 
 
--- tests/test_keplerian_cartesian.cpp
+++ tests/test_keplerian_cartesian.cpp
@@ -1,34 +1,40 @@
 #ifndef TEST_CPP
 #define TEST_CPP
-
+ 
 #include <gtest/gtest.h>
 
 #include "sitellite.hpp"
-#include <iostream>
 #include <Eigen/Dense>
+
+
+
+constexpr double J2 = 1.082'626'68 * 1e-3;
+constexpr double Re = 6'378'137;
+constexpr double GM = 3.986'004'418'888'88 * 1e14;  
+
 
 
 TEST(Keplerian, definition_1) {
     Keplerian A{10., 1./std::sqrt(2), 0.3, 3.};
     
-    ASSERT_NEAR(A.a, 10, 1e-15);
-    ASSERT_NEAR(A.e, 1/sqrt(2), 1e-15);
-    ASSERT_NEAR(A.w, 0.3, 1e-15);
-    ASSERT_NEAR(A.nu, 3., 1e-15);
+    ASSERT_NEAR(A.sem_axis,10, 1e-15);
+    ASSERT_NEAR(A.eccentricity, 1/sqrt(2), 1e-15);
+    ASSERT_NEAR(A.arg_peri, 0.3, 1e-15);
+    ASSERT_NEAR(A.anomaly, 3., 1e-15);
 }
 
 TEST(Keplerian, definition_2) {
     Keplerian A;
 
-    ASSERT_NEAR(A.a, 0., 1e-15);
-    ASSERT_NEAR(A.e, 0., 1e-15);
-    ASSERT_NEAR(A.w, 0., 1e-15);
-    ASSERT_NEAR(A.nu,0., 1e-15);
+    ASSERT_NEAR(A.sem_axis,0., 1e-15);
+    ASSERT_NEAR(A.eccentricity, 0., 1e-15);
+    ASSERT_NEAR(A.arg_peri, 0., 1e-15);
+    ASSERT_NEAR(A.anomaly,0., 1e-15);
 }
 
 TEST(Coordinates, definition) {
-    Eigen::Vector3d A;
-    Eigen::Vector3d B{23., -3, 0.};
+    Eigen::Vector2d A;
+    Eigen::Vector2d B{23., -3};
 
     ASSERT_NEAR(A[0], 0, 1e-15);
     ASSERT_NEAR(A[1], 0, 1e-15);
@@ -37,8 +43,8 @@ TEST(Coordinates, definition) {
 }
 
 TEST(Cartesian, definition_1) {
-    Eigen::Vector3d R(10., 1./std::sqrt(2), 0.);
-    Eigen::Vector3d V(0.3, 3., 0.);
+    Eigen::Vector2d R(10., 1./std::sqrt(2));
+    Eigen::Vector2d V(0.3, 3.);
 
     Cartesian A{R,V};
     
@@ -58,32 +64,33 @@ TEST(Cartesian, definition_2) {
 }
 
 TEST(Cartesian, to_Cartesian_1) {
-    for(double i = 0.; i < M_PI*2 ;i += 0.1)
+    for(double i = - M_PI; i < M_PI;i += M_PI / 15)
     {
-        Keplerian A{10'000'000, 1./2., M_PI + 1, i};
-        Cartesian B = KeplerianToCartesian(A, GM);
-        Keplerian C = CartesianToKeplerian(B,GM);
-        std::cout << "";
+        Keplerian A{10'000'000, 1./2., M_PI - 1, i};
+        
+        Cartesian B = toCartesian(A,GM);
+        Keplerian C = toKeplerian(B,GM);
+        
+        ASSERT_NEAR(A.sem_axis- C.sem_axis,0., 1e-7);
+        ASSERT_NEAR(A.eccentricity - C.eccentricity, 0., 1e-7);
+        
+        
+        ASSERT_NEAR(A.anomaly - C.anomaly, 0. , 1e-10);
+        ASSERT_NEAR(A.arg_peri - C.arg_peri, 0. , 1e-10);
 
-        ASSERT_NEAR(A.a - C.a, 0., 1e-7);
-        ASSERT_NEAR(A.e - C.e, 0., 1e-7);
-        ASSERT_NEAR(A.w - C.w, 0., 1e-1);
-        ASSERT_NEAR(A.nu - C.nu, 0., 1e-5);
     }
 }
 
 TEST(Cartesian, to_Cartesian_2) {
-    for(double i = 0.5; i < M_PI*2 ;i += 0.1)
+    for(double i = - M_PI; i < M_PI ;i += 0.1)
     {
-        Keplerian A{10'000'000, 1./2., 2 * M_PI - i, i};
-        Cartesian B = KeplerianToCartesian(A, GM);
-        Keplerian C = CartesianToKeplerian(B,GM);
-        std::cout <<  "";
-
-        ASSERT_NEAR(A.a - C.a, 0., 1e-3);
-        ASSERT_NEAR(A.e - C.e, 0., 1e-11);
-        ASSERT_NEAR(A.w, C.w, 1e-7);
-        ASSERT_NEAR(A.nu - C.nu, 0., 1e-7);
+        Keplerian A{10'000'000, 1./2., (M_PI - i >= M_PI)?(M_PI-i - 2*M_PI):(M_PI - i), i};
+        Cartesian B = toCartesian(A,GM);
+        Keplerian C = toKeplerian(B,GM);
+        ASSERT_NEAR(A.sem_axis- C.sem_axis,0., 1e-3);
+        ASSERT_NEAR(A.eccentricity - C.eccentricity, 0., 1e-11);
+        ASSERT_NEAR(A.arg_peri, C.arg_peri, 1e-7);
+        ASSERT_NEAR(A.anomaly - C.anomaly, 0., 1e-7);
     }
 }
 
@@ -92,11 +99,11 @@ TEST(Cartesian, to_Keplerian) {
     for(double i = 1.0; i < 1000 ;i += 1.)
 
     {
-        Eigen::Vector3d R{8'000'000., 0., 0.};
-        Eigen::Vector3d V{i, 7500., 0.};
+        Eigen::Vector2d R{8'000'000., 0.};
+        Eigen::Vector2d V{i, 7500.};
         Cartesian A{R,V};
-        Keplerian B = CartesianToKeplerian(A,GM);
-        Cartesian C = KeplerianToCartesian(B,GM);
+        Keplerian B = toKeplerian(A,GM);
+        Cartesian C = toCartesian(B,GM);
         ASSERT_NEAR(A.position[0] - C.position[0], 0., 1e-8);
         ASSERT_NEAR(A.position[1] - C.position[1], 0., 1e-8);
         ASSERT_NEAR(A.velocity[0] - C.velocity[0], 0., 1e-5);
@@ -105,27 +112,26 @@ TEST(Cartesian, to_Keplerian) {
 }
 
 TEST(Cartesian, maneuver) {
-    Eigen::Vector3d R{8'000'000., 0., 0.};
-    Eigen::Vector3d V{0., 7500., 0.};
+    Eigen::Vector2d R{8'000'000., 0.};
+    Eigen::Vector2d V{0., 7500.};
     Cartesian A{R,V};
-    Keplerian B = CartesianToKeplerian(A,GM);
+    Keplerian B = toKeplerian(A,GM);
     
-    Eigen::Vector3d delta_V{0., 10. ,0.};
+    Eigen::Vector2d delta_V{0., 10.};
     Keplerian C =  maneuver(A,delta_V,GM);
-    ASSERT_NEAR((C.a*(1+C.e)-B.a*(1+B.e))/1000 , 63.7487116, 1e-8);
+    ASSERT_NEAR((C.sem_axis*(1+C.eccentricity)-B.sem_axis*(1+B.eccentricity))/1000, 63.7487116, 1e-8);
     
     delta_V[1] -= 20;
-    std::cout << "";
 
-    Cartesian T = KeplerianToCartesian(C,GM);
+    Cartesian T = toCartesian(C,GM);
     C = maneuver(T,delta_V,GM);
-    ASSERT_NEAR((B.a*(1+B.e)-C.a*(1+C.e))/1000, 0., 1e-1);
+    ASSERT_NEAR((B.sem_axis*(1+B.eccentricity)-C.sem_axis*(1+C.eccentricity))/1000, 0., 1e-1);
   
 }
 
 
 TEST(function, precession_rate) {
-    double d = precession_rate(Re + 800'000, 0, 56. / 180. * M_PI);
+    double d = precession_rate(Re + 800'000, 0, 56. / 180. * M_PI, Re, J2, GM);
     ASSERT_NEAR(d, -7.44298 * 1e-7, 1e-12);
 
 }
